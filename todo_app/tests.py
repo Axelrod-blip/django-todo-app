3174fd8fd7d6e93633227e9d4a7fbff23fc071ba
Isaboyev Bexruz Suxrobovich <BIsaboev@beeline.uz>
2025-11-28 00:22:43 +0500

    Initial commit of Django TODO app

---
+++ todo_app/tests.py
@@ -0,0 +1,50 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Todo
+from django.utils import timezone
+
+class TodoTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.todo = Todo.objects.create(
+            title='Test Todo',
+            description='Test Description',
+            due_date=timezone.now()
+        )
+
+    def test_todo_list(self):
+        response = self.client.get(reverse('todo_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Todo')
+
+    def test_todo_create(self):
+        response = self.client.post(reverse('todo_create'), {
+            'title': 'New Todo',
+            'description': 'New Description',
+            'due_date': timezone.now(),
+            'is_resolved': False
+        })
+        self.assertEqual(response.status_code, 302) # Redirects to list
+        self.assertEqual(Todo.objects.count(), 2)
+
+    def test_todo_update(self):
+        response = self.client.post(reverse('todo_update', args=[self.todo.pk]), {
+            'title': 'Updated Todo',
+            'description': 'Updated Description',
+            'due_date': timezone.now(),
+            'is_resolved': False
+        })
+        self.assertEqual(response.status_code, 302)
+        self.todo.refresh_from_db()
+        self.assertEqual(self.todo.title, 'Updated Todo')
+
+    def test_todo_delete(self):
+        response = self.client.post(reverse('todo_delete', args=[self.todo.pk]))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Todo.objects.count(), 0)
+
+    def test_todo_resolve(self):
+        response = self.client.get(reverse('todo_resolve', args=[self.todo.pk]))
+        self.assertEqual(response.status_code, 302)
+        self.todo.refresh_from_db()
+        self.assertTrue(self.todo.is_resolved)
